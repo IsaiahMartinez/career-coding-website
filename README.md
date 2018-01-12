96a7e6ea18d682a7b849c2ce012e49ac4d30d2ae
Isaiah Martinez <imartinez32@cnm.edu>
2018-01-11 20:54:33 -0700

    added goals to milestone-1.php

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # career-coding-website
-my professional software development website for employers
+my professional web development website for employers
--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -2,13 +2,29 @@
 <html lang="en-US">
 	<head>
 		<meta charset="utf-8">
-		<title>Purpose, Audience, Goal</title>
+		<title>tlPurpose, Audience, Goal</title>
 	</head>
 	<body>
-		<h1>Purpose, Audience, Goal</h1>
+		<h1><u>Purpose, Audience, Goal:</u></h1>
 		<ol>
 			<li>
-				<strong>Purpose:</strong> I'm seeking to leverage a proven record of achievement with practical work experience 				as a front-end developer in the tech industry. Always meeting demands, it is my strong desire to exceed them by 				innovating and implementing new concepts and ideas. Through my PWP project, my goal is to demonstate that I am not 				just another employee but an asset to a company
+				<strong>Purpose:</strong> I'm seeking to leverage a proven record of achievement with practical work experience 				as a front-end developer in the tech industry. Always meeting demands, it is my strong desire to exceed them by 				innovating and implementing new concepts and ideas. Through my PWP project, my goal is to demonstate that I am not 				just another employee but an asset to a company.
+			</li>
+			<li>
+				<strong>Audience:</strong> My site is being developed around those seeking to hire web developers. These 				comprise the following:
+				<ul>
+					<li>
+						National Security Agencies such as Sandia National Laboratories, Department of Energy, National Nuclear 						Security Administration, etc.
+					</li>
+					<li>
+						Web development companies, both start-ups and established.
+					</li>
+					<li>
+						New Broadcasting agencies seeking web developers for content management.
+					</li>
+				</ul>
+			<li>
+				<strong>Goal:</strong> My goal is to gain employment or an internship opportunity in web development.
 			</li>
 		</ol>
 	</body>
