858497983b03dbb85c8518a6325f9db80fe21312
Isaiah Martinez <imartinez32@cnm.edu>
2018-01-11 20:18:36 -0700

    added purpose statement

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<meta charset="utf-8">
+		<title>Purpose, Audience, Goal</title>
+	</head>
+	<body>
+		<h1>Purpose, Audience, Goal</h1>
+		<ol>
+			<li>
+				<strong>Purpose:</strong> I'm seeking to leverage a proven record of achievement with practical work experience 				as a front-end developer in the tech industry. Always meeting demands, it is my strong desire to exceed them by 				innovating and implementing new concepts and ideas. Through my PWP project, my goal is to demonstate that I am not 				just another employee but an asset to a company
+			</li>
+		</ol>
+	</body>
+</html>
