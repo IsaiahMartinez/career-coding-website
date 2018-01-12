4256621b6474859e3478c553d0f64866cca4f49f
Isaiah Martinez <imartinez32@cnm.edu>
2018-01-11 21:34:14 -0700

    added persona information including name, age, job/role, and technology utilized

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -2,10 +2,11 @@
 <html lang="en-US">
 	<head>
 		<meta charset="utf-8">
-		<title>tlPurpose, Audience, Goal</title>
+		<title>Purpose, Audience, Goal</title>
 	</head>
 	<body>
-		<h1><u>Purpose, Audience, Goal:</u></h1>
+		<h1><u>PWP Milestone One:</u></h1>
+		<h2>Purpose, Audience, Goal:</h2>
 		<ol>
 			<li>
 				<strong>Purpose:</strong> I'm seeking to leverage a proven record of achievement with practical work experience 				as a front-end developer in the tech industry. Always meeting demands, it is my strong desire to exceed them by 				innovating and implementing new concepts and ideas. Through my PWP project, my goal is to demonstate that I am not 				just another employee but an asset to a company.
@@ -27,5 +28,25 @@
 				<strong>Goal:</strong> My goal is to gain employment or an internship opportunity in web development.
 			</li>
 		</ol>
+		<h2>Persona:</h2>
+		<ul>
+			<li>
+				<strong>Name:</strong> Jack Lauer
+			</li>
+			<li>
+				<strong>Demographic Information:</strong>
+			</li>
+				<ul>
+					<li>
+						<strong>Age:</strong> 56
+					</li>
+					<li>
+						<strong>Job/Role:</strong> Manager - Space and Asset Management
+					</li>
+					<li>
+						<strong>Technology Utilized:</strong> iPhone 8 and Dell Inspiron 17 7000 with docking station.
+					</li>
+				</ul>
+		</ul>
 	</body>
 </html>
