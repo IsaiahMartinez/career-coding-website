c43b4f0f6d3877e6f1887d1fcf9ab4c519278e30
Isaiah Martinez <imartinez32@cnm.edu>
2018-01-15 09:58:06 -0700

    corrected <li></li> indentations

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -8,44 +8,23 @@
 		<h1><u>PWP Milestone One:</u></h1>
 		<h2>Purpose, Audience, Goal:</h2>
 		<ol>
-			<li>
-				<strong>Purpose:</strong> I'm seeking to leverage a proven record of achievement with practical work experience 				as a front-end developer in the tech industry. Always meeting demands, it is my strong desire to exceed them by 				innovating and implementing new concepts and ideas. Through my PWP project, my goal is to demonstate that I am not 				just another employee but an asset to a company.
-			</li>
-			<li>
-				<strong>Audience:</strong> My site is being developed around those seeking to hire web developers. These 				comprise the following:
+			<li><strong>Purpose:</strong> I'm seeking to leverage a proven record of achievement with practical work experience 				as a front-end developer in the tech industry. Always meeting demands, it is my strong desire to exceed them by 				innovating and implementing new concepts and ideas. Through my PWP project, my goal is to demonstate that I am  				not just another employee but an asset to a company.</li>
+			<li><strong>Audience:</strong> My site is being developed around those seeking to hire web developers. These 				comprise the following:
 				<ul>
-					<li>
-						National Security Agencies such as Sandia National Laboratories, Department of Energy, National Nuclear 						Security Administration, etc.
-					</li>
-					<li>
-						Web development companies, both start-ups and established.
-					</li>
-					<li>
-						New Broadcasting agencies seeking web developers for content management.
-					</li>
+					<li>National Security Agencies such as Sandia National Laboratories, Department of Energy, National Nuclear 						Security Administration, etc.</li>
+					<li>Web development companies, both start-ups and established.</li>
+					<li>New Broadcasting agencies seeking web developers for content management.</li>
 				</ul>
-			<li>
-				<strong>Goal:</strong> My goal is to gain employment or an internship opportunity in web development.
-			</li>
+			<li><strong>Goal:</strong> My goal is to gain employment or an internship opportunity in web development.</li>
 		</ol>
 		<h2>Persona:</h2>
 		<ul>
-			<li>
-				<strong>Name:</strong> Jack Lauer
-			</li>
-			<li>
-				<strong>Demographic Information:</strong>
-			</li>
+			<li><strong>Name:</strong> Jack Lauer</li>
+			<li><strong>Demographic Information:</strong></li>
 				<ul>
-					<li>
-						<strong>Age:</strong> 56
-					</li>
-					<li>
-						<strong>Job/Role:</strong> Manager - Space and Asset Management
-					</li>
-					<li>
-						<strong>Technology Utilized:</strong> iPhone 8 and Dell Inspiron 17 7000 with docking station.
-					</li>
+					<li><strong>Age:</strong> 56</li>
+					<li><strong>Job/Role:</strong> Manager - Space and Asset Management</li>
+					<li><strong>Technology Utilized:</strong> iPhone 8 and Dell Inspiron 17 7000 with docking station.</li>
 				</ul>
 		</ul>
 	</body>
