9c15968e9841878ccc578e13fa91c9499139340f
Isaiah Martinez <imartinez32@cnm.edu>
2018-01-15 14:48:57 -0700

    added frustrations and needs.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -8,10 +8,12 @@
 		<h1><u>PWP Milestone One:</u></h1>
 		<h2>Purpose, Audience, Goal:</h2>
 		<ol>
-			<li><strong>Purpose:</strong> I'm seeking to leverage a proven record of achievement with practical work experience 				as a front-end developer in the tech industry. Always meeting demands, it is my strong desire to exceed them by 				innovating and implementing new concepts and ideas. Through my PWP project, my goal is to demonstate that I am  				not just another employee but an asset to a company.</li>
+			<li><strong>Purpose:</strong> I'm seeking to leverage a proven record of achievement with practical work
+				experience as a front-end developer in the tech industry. Always meeting demands, it is my strong desire to 				exceed them by innovating and implementing new concepts and ideas. Through my PWP project, my goal is to 				demonstate that I am not just another employee but an asset to a company.</li>
 			<li><strong>Audience:</strong> My site is being developed around those seeking to hire web developers. These 				comprise the following:
 				<ul>
-					<li>National Security Agencies such as Sandia National Laboratories, Department of Energy, National Nuclear 						Security Administration, etc.</li>
+					<li>National Security Agencies such as Sandia National Laboratories, Department of Energy, National
+						Nuclear Security Administration, etc.</li>
 					<li>Web development companies, both start-ups and established.</li>
 					<li>New Broadcasting agencies seeking web developers for content management.</li>
 				</ul>
@@ -24,13 +26,22 @@
 				<ul>
 					<li><strong>Age:</strong> 56</li>
 					<li><strong>Job/Role:</strong> Manager - Space and Asset Management. Manages three different departments
-					within facilites at Sandia National Laboratories. The management team would like to hire a full-time, 					Front-End Web Developer to build and maintain a website that showcases the departments accomplishments and 					provides information on new and ongoing projects.</li>
+					within facilites at Sandia National Laboratories. The management team would like to hire a full-time, 					Front-End Web Developer to build and maintain a website that showcases the departments accomplishments
+					and provides information on new and ongoing projects.</li>
 					<li><strong>Technology Utilized:</strong> iPhone 8 and Dell Inspiron 17 7000 with docking station.</li>
 					<li><strong>Attitudes and Behaviors:</strong> Mr. Lauer has a very busy work schedule with meetings
 					throughout the day. When it comes to computers and software programs, his technical knowledge is limited.
 					Although he relies on his employees to help him navigate programs he has never used, once he is shown,
-					he catches on pretty quickly and usually only needs to be shown once or twice. Mr. Lauer doesn't use social 					media so when looking to hire, he prefers electronic resumes and a link to a portfolio website so he can 					review qualifications.</li>
-					<li><strong>Frustrations and Needs:</strong> Mr. Lauer and his team are looking for a candidate who is 					not limited to web development but has a varied work history. Although </li>
+					he catches on pretty quickly and usually only needs to be shown once or twice. Mr. Lauer doesn't use
+					social media so when looking to hire, he prefers electronic resumes and a link to a portfolio website
+					so he can review qualifications.</li>
+					<li><strong>Frustrations and Needs:</strong> Mr. Lauer and his team are looking for a candidate who is 					not limited to web development but has a varied work history. Although the Web Developer role is a main
+					aspect of the position, he also needs someone who is comfortable learning other proprietary software
+					programs as business needs develop. Additionally, he would prefer someone with some advanced Excel
+					experience (pivot tables, v-lookups, etc.) to assist with data trending. Mr. Lauer's team is very
+					close knit so soft skills are very important; he has had to let 2 previous employees go due to their 					unprofessionalism. Mr. Lauer is willing to hire someone with less experience if that person is reliable, 					trainable and he feels like they would be a good addition to the team. He would like to hire someone
+					as soon as possible. Salary range is between $50k to $58k.
+					</li>
 				</ul>
 		</ul>
 	</body>
